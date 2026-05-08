78816dd3afd08c817b6a1a23a25b880c88b07946
Nathanael Sagala <nsagala@students.cs.ubc.ca>
2026-05-09 05:50:35 +0700

    Start Button logic

--- public/game.js
+++ public/game.js
@@ -4,3 +4,30 @@ const result = document.getElementById('result');
 const startBtn = document.getElementById('startBtn');
 const playerName = document.getElementById('playerName');
 const scoreList = document.getElementById('scoreList');
+
+let startTime = null;
+let waitTimeout = null;
+let gameActive = false;
+
+startBtn.addEventListener('click', function() {
+    const name = playerName.ariaValueMax.trim();
+
+    if (name === '') {
+        message.textContent = 'Please enter your name first!';
+        return;
+    }
+
+    startBtn.disabled = true;
+    result.textContent = '';
+    circle.className = 'circle ready';
+    message.textContent = 'Get ready ...';
+
+    const delay = Math.random() * 3000 + 1000;
+
+    waitTimeout = setTimeout(function() {
+        circle.className = 'circle go';
+        message.textContent = 'CLICK NOW!';
+        startTime = Date.now;
+        gameActive = true;
+    }, delay)
+});
