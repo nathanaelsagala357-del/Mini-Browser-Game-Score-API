1ffc769a5fb724ac79b05d83d756700c4ecb9394
Nathanael Sagala <nsagala@students.cs.ubc.ca>
2026-05-09 05:42:38 +0700

    HTML elements to js

---
+++ public/game.js
@@ -0,0 +1,6 @@
+const circle = document.getElementById('circle');
+const message = document.getElementById('message');
+const result = document.getElementById('result');
+const startBtn = document.getElementById('startBtn');
+const playerName = document.getElementById('playerName');
+const scoreList = document.getElementById('scoreList');
