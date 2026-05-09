d473b403c81508664c0637aa9d74d44dee1d888d
Nathanael Sagala <nsagala@students.cs.ubc.ca>
2026-05-09 07:00:38 +0700

    js server const set up

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
