001b71281212a5dda036ce59e5c3301021c9fe91
Nathanael Sagala <nsagala@students.cs.ubc.ca>
2026-05-09 07:10:19 +0700

    The API routes update

--- server.js
+++ server.js
@@ -7,3 +7,25 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+let scores = [];
+
+app.post('/scores', function(req, res) {
+    const { name, time} = req.body();
+
+    if (!name || !time) {
+        return res.status(400).json({ error: 'Name and time are required'});
+    }
+
+    scores.push({ name: name, time: time});
+    scores.sort(function(a, b) {
+        return a.time - b.time;
+    });
+    scores = scores.slice(0, 5);
+
+    res.json({ message: 'Score saved!', scores: scores});
+});
+
+app.get('/scores', function(req, res) {
+    res.json(scores);
+});
